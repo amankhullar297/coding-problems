812beb21d0fcb76d38207f161b89a4c3bd962798
Aman Khullar <amankhullar297@gmail.com>
2021-12-02 23:21:54 +0530

    Added Common subsequence problem.

--- src/DP/CommonSubSequence/CommonSubSequenceProblem.java
+++ src/DP/CommonSubSequence/CommonSubSequenceProblem.java
@@ -1,7 +1,7 @@
 /*
 Given two strings ‘X’ and ‘Y’, find the length of the longest common substring.
 
-Approach: Start checking each characters. If characters matches, then increment count and check for rest characters in both
+Approach: Start comparing each characters. If characters matches, then increment count and check for rest characters in both
 strings and if character doesnot match then compare first string by removing current character from other string and also compare other string by
 removing current character from first string.
 
